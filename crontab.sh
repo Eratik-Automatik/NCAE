8300293a2a53ec62ec46d92de03717c7dd3a31f3
Eratik Automatik <phackl1@wgu.edu>
2023-03-08 00:23:13 -0600

    Update crontab.sh

--- crontab.sh
+++ crontab.sh
@@ -1,1 +1,15 @@
+check_crontab_func () {
+#Create user list and pull each user
+cat /etc/passwd | cut -d: -f1 > user_list.txt
+FILE="userlist.txt"
 
+for i in `cat user_list.txt`
+do
+  echo "###### $i crontab is:"
+  crontab -u $i -l
+  
+done >> /home/default/Documents/cronjobs.txt
+
+}
+
+check_crontab_func
