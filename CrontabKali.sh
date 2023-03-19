58945b9e5c8607dbbd950a19770974101329604f
Eratik Automatik <phackl1@wgu.edu>
2023-03-18 20:43:24 -0500

    Update CrontabKali.sh

--- CrontabKali.sh
+++ CrontabKali.sh
@@ -1,7 +1,13 @@
 check_crontab_func () {
 #Create user list and pull each user
+touch cronjobs.txt
+sudo chmod 777 cronjobs.txt
+
 cat /etc/passwd | cut -d: -f1 > user_list.txt
 FILE="userlist.txt"
+
+
+sudo chmod 777 cronjobs.txt
 
 for i in `cat user_list.txt`
 do
