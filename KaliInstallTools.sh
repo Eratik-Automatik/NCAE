8a8f6c0121f1924ea4f623b5683907842636c182
WGU-NCAE <128280131+WGU-NCAE@users.noreply.github.com>
2023-03-21 19:41:58 -0500

    Initial Code Commit

--- KaliInstallTools.sh
+++ KaliInstallTools.sh
@@ -1,1 +1,34 @@
+install_tools_func () {
 
+	# Added in error testing, who knows what gets borked
+	# per https://www.linux.org/docs/man5/os-release.html
+	# /usr/lib/os-release should be the fallback
+
+	if . /etc/os-release ; then
+	        OS=$NAME
+	else
+	        . /usr/lib/os-release
+	        OS=$NAME
+	fi
+
+	echo "$OS installing tools"
+
+	if [ "$OS" = "Ubuntu" ]; then
+		apt -y install fail2ban tripwire clamav inotify-tools
+		#echo "Ubuntu"
+
+	elif [ "$OS" = "Kali GNU/Linux" ]; then
+		apt -y install fail2ban tripwire clamav inotify-tools
+		#echo "Kali"
+
+	elif [ "$OS" = "CentOS Linux" ];then
+		yum -y install fail2ban tripwire clamav inotify-tools
+		#echo "CentOS"
+
+	else 
+	echo "Not Ubuntu, Debian or CentOS, install tools manually"
+
+	fi
+}
+
+install_tools_func
